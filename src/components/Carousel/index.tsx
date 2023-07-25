196f090d55a471377b0fe7198e0101ba729710cf
Britosl1 <lucasbrito.carneiro@gmail.com>
2023-07-25 00:23:20 -0300

    [feat]: update carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -14,19 +14,19 @@ export default function Carousel({
   disabled,
 }: ICarouselProps) {
   return (
-    <div>
+    <div className="carosel-container">
+      <button
+        className={
+          disabled ? "buttons-slider-container-disabled" : "button-abled-left"
+        }
+        onClick={onScrollLeft}
+        disabled={disabled}
+      >
+        &#12296;
+      </button>
       {children}
       <div className="buttons-slider-container">
-        <button
-          className={
-            disabled ? "buttons-slider-container-disabled" : "button-abled"
-          }
-          onClick={onScrollLeft}
-          disabled={disabled}
-        >
-          &#12296;
-        </button>
-        <button className="button-abled" onClick={onScrollRight}>
+        <button className="button-abled-right" onClick={onScrollRight}>
           &#12297;
         </button>
       </div>
