1a19c5abf4d76fd751b0c72e6e44ca61f717fd1c
Britosl1 <lucasbrito.carneiro@gmail.com>
2023-07-24 23:03:34 -0300

    [feat]: update post method

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import GameCard from "./components/GameCard";
 import { getGames } from "./services/games";
 import { IGamesProps } from "./interfaces/gameInterfaces";
 
+import "./App.css";
+
 function App() {
   const [gameData, setGameData] = useState<IGamesProps[]>([]);
-  const fetchGames = async () => {
+  // const [caroselNumber, setCaroselNumber] = useState<number>(6);
+
+  const fetchGames = useCallback(async () => {
     try {
-      const data = await getGames();
+      const data = await getGames({});
       setGameData(data.result.games);
     } catch (error) {
       console.log(error);
     }
-  };
+  }, []);
 
   console.log(gameData);
 
   useEffect(() => {
     fetchGames();
-  }, []);
+  }, [fetchGames]);
+
+  console.log(gameData);
+
   return (
-    <div>
-      {gameData.map((game: IGamesProps) => (
+    <div className="home-container">
+      {/* {gameData.splice(0, 6).map((game: IGamesProps) => ( */}
+      {gameData.splice(0, 6).map((game: IGamesProps) => (
         <GameCard
           name={game.name}
           icon={game.icon}
--- src/services/games.ts
+++ src/services/games.ts
@@ -1,12 +1,22 @@
 const apiUrl = "https://api-casino.recois.com/slots/list";
 
-export const getGames = async () => {
+type Sort = "a-z" | "z-a" | "release";
+
+interface GetGamesResponse {
+  sort_by?: string;
+  search?: string;
+  num?: number;
+  page_num?: number;
+}
+
+export const getGames = async (params: GetGamesResponse) => {
   const res = await fetch(apiUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      accept: "application.json",
+      accept: "application/json",
     },
+    body: JSON.stringify(params),
   });
 
   return res.json();
